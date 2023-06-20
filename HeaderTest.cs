f93721f14d201f3c2c59c3d7db845eadccf95f90
Meerim <mekabolzhirova@gmail.com>
2023-06-20 16:23:22 -0400

    added my first class

---
+++ HeaderTest.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chromer;
+
+public class Class1
+{
+	[Test]
+	public void validatLogo()
+	{
+		IWebDriver driver = new ChromeDriver(); 
+	}
+}
